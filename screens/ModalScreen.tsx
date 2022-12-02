6dda83ea8e4c7f14e68db5320c9c609ed5249b21
Mac_Developer <araozangel842@gmail.com>
2022-12-02 17:15:20 +0200

    Render modal as user profile
    Also, shows all users who have made contact

--- screens/ModalScreen.tsx
+++ screens/ModalScreen.tsx
@@ -1,15 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { Image, Platform, StyleSheet, Text } from 'react-native';
+import { View } from '../components/Themed';
+import event from '../assets/data/event.json';
+import users from '../assets/data/users.json';
+import { AntDesign } from '@expo/vector-icons';
+import CustomButton from '../components/CustomButton';
 
 export default function ModalScreen() {
+  const onJoin = () => {
+    console.warn('Join');
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/ModalScreen.tsx" />
+      <Text style={styles.title}>{event.name}</Text>
+      <Text style={styles.time}>
+        <AntDesign name="calendar" size={24} color="black" />
+        {'  '}
+        {new Date(event.date).toDateString()}
+      </Text>
+      <View style={styles.footer}>
+        {/*  User avatars */}
+        <View style={styles.user}>
+          {users.map((user) => (
+            <Image style={styles.avatar} source={{ uri: user.avatarUrl }} />
+          ))}
+        </View>
+        <CustomButton text="Join" onPress={onJoin} />
+      </View>
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
@@ -20,16 +38,29 @@ export default function ModalScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingLeft: 12,
+    paddingRight: 12,
+    paddingTop: 15,
+    paddingBottom: 25,
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    marginVertical: 10,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  time: {
+    color: '#5c5e62',
+    fontSize: 20,
   },
+  footer: {
+    marginTop: 'auto',
+  },
+  user: {
+    flexDirection: 'row',
+  },
+  avatar: {
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 25,
+  }
 });
