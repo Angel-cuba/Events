c81a431a83609a50f73e04cb5abe3a4a614144ea
Mac_Developer <araozangel842@gmail.com>
2022-12-07 17:33:52 +0200

    More types

--- types.tsx
+++ types.tsx
@@ -31,6 +31,7 @@ export type RootTabParamList = {
   TabOne: undefined;
   TabTwo: undefined;
   SignIn: undefined;
+  SignUp: undefined;
 };
 
 export type RootTabScreenProps<Screen extends keyof RootTabParamList> = CompositeScreenProps<
