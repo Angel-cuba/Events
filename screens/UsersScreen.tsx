b3a81c06f9fd29523a004b1492150591b226c2fd
Mac_Developer <araozangel842@gmail.com>
2022-12-13 18:47:33 +0200

    Fetch users from database

--- screens/UsersScreen.tsx
+++ screens/UsersScreen.tsx
@@ -1,14 +1,27 @@
-import users from '../assets/data/users.json';
-import { FlatList } from 'react-native';
-import UserItem from '../components/UsersItem'
+import { ActivityIndicator, FlatList, Text } from 'react-native';
+import UserItem from '../components/UsersItem';
+import { gql, useQuery } from '@apollo/client';
 
+  const getUsersQuery = gql`
+    query {
+      users {
+        id
+        displayName
+        avatarUrl
+      }
+    }
+  `;
 const UsersScreen = () => {
-  return (
-    <FlatList
-      data={users}
-      renderItem={({ item }) => <UserItem user={item} />}
-    />
-  );
+  const { data, loading, error } = useQuery(getUsersQuery);
+
+  if (loading) {
+    return <ActivityIndicator />;
+  }
+  if (error) {
+    return <Text>{error.message}</Text>;
+  }
+
+  return <FlatList data={data.users} renderItem={({ item }) => <UserItem user={item} />} />;
 };
 
 export default UsersScreen;
